ad469a2fb334722221d00cc180d95552f07c26eb
winnie279 <57951904+Yuji-207@users.noreply.github.com>
2025-03-09 23:09:33 +0900

    chore: Clear git cache

--- app/database/sql/CREATE_SUPPLIES_TABLE.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS Supplies;
-CREATE TABLE IF NOT EXISTS Supplies (
-  supply_id INTEGER PRIMARY KEY,
-  category TEXT,
-  item_name TEXT,
-  expiration_date TEXT,
-  created_datetime DATETIME,
-  updated_datetime DATETIME
-);
